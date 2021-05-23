e96961d063cbc8d1b7232dac39344d49e19ff431
Daniel Marques <daniel.brz2009@gmail.com>
2021-05-23 01:22:08 -0300

    Created project scaffold

---
+++ README.md
@@ -0,0 +1,1 @@
+# challenge-tcesp-fiap-microservicos
---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+import createConnection from './database';
+import { router } from './routes';
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+export { app };
